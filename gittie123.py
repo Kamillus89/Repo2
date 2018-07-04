741c771058a6ad83440994ae3f07ed8e5b6fe20a
Kamil <kamil.m.olsza@gmail.com>
2018-07-04 11:41:20 +0200

    Commit_full_version

--- gittie123.py
+++ gittie123.py
@@ -3,7 +3,6 @@ def introduce():
 
 introduce()
 
-<<<<<<< HEAD
 def add(a,b):
         a = float(a)
         b = float(b)
@@ -12,10 +11,9 @@ def add(a,b):
 a = input("Podaj a = ")
 b = input("Podaj b = ")
 add(a,b)
-=======
+
 def joke():
     print ('Ile nici ma dentysta?')
     print ('Stomatologiczne')
     print ('hahaha')
 joke()
->>>>>>> 92939eb88ca87bb39a08124e77e7bb6443246a70
