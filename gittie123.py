13cc4b2926c5df6648f60f79891052d32932efad
Kamil <kamil.m.olsza@gmail.com>
2018-07-04 11:27:40 +0200

    Commit_add

--- gittie123.py
+++ gittie123.py
@@ -2,3 +2,12 @@ def introduce():
     print ('Hello, I\'m Gittie2!')
 
 introduce()
+
+def add(a,b):
+        a = float(a)
+        b = float(b)
+        print(a+b)
+
+a = input("Podaj a = ")
+b = input("Podaj b = ")
+add(a,b)
